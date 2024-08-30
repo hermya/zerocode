4ff54bd4450bff85e93f8cdeba81aa17d3b75329
Heramb Joshi <herambku@gmail.com>
2024-08-30 11:20:36 -0500

    Fixed flaky test-case in the following class: ConsumerJsonRecordTest.should_serialize_a_record_with_headers POINT OF FAILURE -> random attribute ordering in hashmap Fixed using NonDex, plugin added in parent pom. Suggested command to check flaky tests : > mvn nondex:nondex
    For particular test:
    >mvn -pl ./core nondex:nondex -Dtest=org.jsmart.zerocode.core.kafka.receive.message.ConsumerJsonRecordTest#should_serialize_a_record_with_headers
    
    For more information : https://github.com/TestingResearchIllinois/NonDex

--- core/src/test/java/org/jsmart/zerocode/core/kafka/receive/message/ConsumerJsonRecordTest.java
+++ core/src/test/java/org/jsmart/zerocode/core/kafka/receive/message/ConsumerJsonRecordTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -49,7 +50,7 @@ public class ConsumerJsonRecordTest {
         // given
         JsonNode key = objectMapper.readTree("123");
         JsonNode value = objectMapper.readTree("\"val\"");
-        Map<String, String> headers = new HashMap<>();
+        Map<String, String> headers = new LinkedHashMap<>();
         headers.put("hKey", "hValue");
         headers.put("hKeyWithNullValue", null);
         ConsumerJsonRecord record = new ConsumerJsonRecord(key, value, headers);
